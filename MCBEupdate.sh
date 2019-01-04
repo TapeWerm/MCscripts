387ecdcdbd2954938e8edceacfd8eb8c321a6068
TapeWerm <vverm@cat.pdx.edu>
2019-01-03 22:58:15 -0800

    Add $backup_dir to MCBEupdate.sh
    Move backups to /tmp in MCBEupdate.sh

--- MCBEupdate.sh
+++ MCBEupdate.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+backup_dir=/tmp
 files='worlds whitelist.json permissions.json server.properties'
 
 if [ -z "$1" -o -z "$2" -o "$1" = -h -o "$1" = --help ]; then
 	>&2 echo Updates Minecraft Bedrock Edition server keeping worlds, whitelist, permissions, and properties.
 	>&2 echo '`./MCBEupdate.sh $server_dir $minecraft_zip`'
-	>&2 echo '$server_dir cannot be ~. Remember to stop server before updating.'
+	>&2 echo Remember to stop server before updating.
 	exit 1
 fi
 
 server_dir=${1%/}
 # Remove trailing slash
-if [ "$server_dir" -ef ~ ]; then
-	>&2 echo '$server_dir cannot be ~'
+if [ "$server_dir" -ef $backup_dir ]; then
+	>&2 echo '$server_dir cannot be '$backup_dir
 	exit 4
 fi
 server_dir=`realpath "$server_dir"`
@@ -38,12 +39,12 @@ for file in $files; do
 		>&2 echo No file $file
 		exit 3
 	fi
-	mv $file ~
+	mv $file $backup_dir/
 done
 
 rm -r $server_dir/*
 unzip $minecraft_zip
 
 for file in $files; do
-	mv ~/$file .
+	mv $backup_dir/$file .
 done
