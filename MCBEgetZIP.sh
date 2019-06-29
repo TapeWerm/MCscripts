b3618864088843f0bfe1ec6d6df2cc27ae46f57f
TapeWerm <TapeWerm@users.noreply.github.com>
2019-06-29 01:02:14 -0700

    Prefer IPv4 instead of force

--- MCBEgetZIP.sh
+++ MCBEgetZIP.sh
@@ -17,7 +17,7 @@ if [ "$#" -gt 1 ]; then
 	exit 1
 fi
 
-webpage=$(wget https://www.minecraft.net/en-us/download/server/bedrock/ -O -)
+webpage=$(wget --prefer-family=IPv4 https://www.minecraft.net/en-us/download/server/bedrock/ -O -)
 url=$(echo "$webpage" | grep -Eo 'https://[^ ]+bin-linux/bedrock-server-[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.zip')
 current_ver=$(basename "$url")
 installed_ver=$(ls ~mc/bedrock-server-[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.zip 2> /dev/null || true)
@@ -26,7 +26,7 @@ installed_ver=$(ls ~mc/bedrock-server-[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.zip 2> /de
 if ! echo "$installed_ver" | grep -q "$current_ver"; then
 # There might be more than one ZIP in ~mc
 	trap 'sudo rm -f ~mc/"$current_ver"' ERR
-	sudo wget -4 "$url" -O ~mc/"$current_ver"
+	sudo wget --prefer-family=IPv4 "$url" -O ~mc/"$current_ver"
 	trap - ERR
 	# Do not remove $current_ver if wget succeeded, below fails will repeat
 	sudo chown -R mc:nogroup ~mc/"$current_ver"
