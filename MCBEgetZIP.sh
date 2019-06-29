1652c8235b28abca5e03ccaa1883b08f34a8085c
TapeWerm <TapeWerm@users.noreply.github.com>
2019-06-29 01:37:52 -0700

    Make MCBEgetZIP independent of mc user

--- MCBEgetZIP.sh
+++ MCBEgetZIP.sh
@@ -6,7 +6,7 @@ syntax='`./MCBEgetZIP.sh`'
 
 case $1 in
 --help|-h)
-	echo "If the ZIP of the current version isn't in ~mc, download it, and remove outdated ZIPs in ~mc."
+	echo "If the ZIP of the current version isn't in ~, download it, and remove outdated ZIPs in ~."
 	echo "$syntax"
 	exit
 	;;
@@ -20,15 +20,14 @@ fi
 webpage=$(wget --prefer-family=IPv4 https://www.minecraft.net/en-us/download/server/bedrock/ -O -)
 url=$(echo "$webpage" | grep -Eo 'https://[^ ]+bin-linux/bedrock-server-[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.zip')
 current_ver=$(basename "$url")
-installed_ver=$(ls ~mc/bedrock-server-[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.zip 2> /dev/null || true)
+installed_ver=$(ls ~/bedrock-server-[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.zip 2> /dev/null || true)
 # ls fails if there's no match
 
 if ! echo "$installed_ver" | grep -q "$current_ver"; then
-# There might be more than one ZIP in ~mc
-	trap 'sudo rm -f ~mc/"$current_ver"' ERR
-	sudo wget --prefer-family=IPv4 "$url" -O ~mc/"$current_ver"
+# There might be more than one ZIP in ~
+	trap 'sudo rm -f ~/"$current_ver"' ERR
+	wget --prefer-family=IPv4 "$url" -O ~/"$current_ver"
 	trap - ERR
 	# Do not remove $current_ver if wget succeeded, below fails will repeat
-	sudo chown -R mc:nogroup ~mc/"$current_ver"
-	sudo rm -f $installed_ver
+	rm -f $installed_ver
 fi
