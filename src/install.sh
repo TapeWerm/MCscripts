85553932fd400a40f080eeced0579a3fb3412f1a
Vincent Vermilya <TapeWerm@users.noreply.github.com>
2025-06-29 18:44:58 -0700

    Remove dependency python3-bs4

--- src/install.sh
+++ src/install.sh
@@ -68,7 +68,7 @@ fi
 if command -v apt-get &> /dev/null; then
 	apt-get update
 	apt-get install -y curl dosfstools html-xml-utils socat zip
-	apt-get install -y python3-bs4 python3-requests python3-systemd python3-toml
+	apt-get install -y python3-requests python3-systemd python3-toml
 fi
 if ! id mc &> /dev/null; then
 	useradd -rmd /opt/MC -s /usr/sbin/nologin mc
