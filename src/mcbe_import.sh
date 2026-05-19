f6e897649ac55f4a27ee5735f77cd488fc811cd9
Vincent Vermilya <TapeWerm@users.noreply.github.com>
2026-05-19 00:47:10 -0700

    Fix mcbe_import.sh sed syntax

--- src/mcbe_import.sh
+++ src/mcbe_import.sh
@@ -84,7 +84,7 @@ mkdir -p "$mcscripts_dir"
 # Convert DOS line endings to UNIX line endings
 for file in "$server_dir"/*.{json,properties}; do
 	if [ -f "$file" ]; then
-		sed -ie 's/\r$//' "$file"
+		sed -i -e 's/\r$//' "$file"
 	fi
 done
 chown -R mc:mc "$server_dir"
